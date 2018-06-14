bc8a1429b94a414a85a68ea84bcd9cb8d8b59b2e
yangsheng <1923025333@qq.com>
2018-06-14 12:11:31 +0800

    add adapter library

---
+++ adapterlib/src/main/java/com/jdqm/adapterlib/CommonAdapter.java
@@ -0,0 +1,48 @@
+package com.jdqm.adapterlib;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+
+import com.jdqm.adapterlib.base.ItemViewDelegate;
+import com.jdqm.adapterlib.base.ViewHolder;
+
+import java.util.List;
+
+/**
+ * Created by Jdqm on 2018-6-13.
+ */
+public abstract class CommonAdapter<T> extends MultiItemTypeAdapter<T> {
+    protected Context mContext;
+    protected int mLayoutId;
+    protected List<T> mDatas;
+    protected LayoutInflater mInflater;
+
+    public CommonAdapter(final Context context, final int layoutId, List<T> datas) {
+        super(context, datas);
+        mContext = context;
+        mInflater = LayoutInflater.from(context);
+        mLayoutId = layoutId;
+        mDatas = datas;
+
+        addItemViewDelegate(new ItemViewDelegate<T>() {
+            @Override
+            public int getItemViewLayoutId() {
+                return layoutId;
+            }
+
+            @Override
+            public boolean isForViewType(T item, int position) {
+                return true;
+            }
+
+            @Override
+            public void convert(ViewHolder holder, T t, int position) {
+                CommonAdapter.this.convert(holder, t, position);
+            }
+        });
+    }
+
+    protected abstract void convert(ViewHolder holder, T t, int position);
+
+
+}
---
+++ adapterlib/src/main/java/com/jdqm/adapterlib/base/ItemViewDelegate.java
@@ -0,0 +1,15 @@
+package com.jdqm.adapterlib.base;
+
+
+/**
+ * Created by Jdqm on 2018-6-13.
+ */
+public interface ItemViewDelegate<T> {
+
+    int getItemViewLayoutId();
+
+    boolean isForViewType(T item, int position);
+
+    void convert(ViewHolder holder, T t, int position);
+
+}
